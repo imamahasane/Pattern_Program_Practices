7bc06434f679ae5039c280815545d757c7f4e13f
Md Imam Ahasan <emamahasane@gmail.com>
2021-12-20 00:27:15 +0600

    Add files via upload

--- 12_triangle_number_pattern.py
+++ 12_triangle_number_pattern.py
@@ -3,6 +3,7 @@ n = int(input("Please input hear (Number only): "))
 for i in range(n):
     for j in range(n - i):
         print(" ", end=" ")
+    
     for k in range(i+1):
-        print(k+1, end=" ")
+        print(n-k, end=" ")
     print()
--- 9_triangle_number_pattern.py
+++ 9_triangle_number_pattern.py
@@ -1,8 +1,8 @@
 n = int(input("Please input hear (Number only): "))
 
 for i in range(n):
-    for j in range(n - i):
+    for j in range(n-i):
         print(" ", end=" ")
-    for k in range(i+1):
+    for k in range(i,-1,-1):
         print(k+1, end=" ")
     print()
